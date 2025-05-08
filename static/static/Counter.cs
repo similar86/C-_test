8b6fbea661d74bdc8e8a6e3fa759db24ac4939b2
similar86 <40771801+similar86@users.noreply.github.com>
2025-05-08 16:30:53 +0900

    ProgramとCounterを改修

--- static/static/Counter.cs
+++ static/static/Counter.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
  
@@ -9,7 +10,7 @@ namespace Problemex2_4
     class Counter
     {
         //  カウント
-        private int count = 1;
+        private int count = 0;
         //  コンストラクタ
         public void Reset()
         {
