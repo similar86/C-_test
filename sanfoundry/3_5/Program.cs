fd12295efc284f4b2e4efa434659574477e6a579
similar86 <40771801+similar86@users.noreply.github.com>
2025-05-12 14:19:00 +0900

    sanfoundryの3と5の倍数の和を求めるコードの実装

---
+++ sanfoundry/3_5/Program.cs
@@ -0,0 +1,21 @@
+﻿//using Internal;
+// See https://aka.ms/new-console-template for more information
+using System;
+class program
+{
+    public static void Main()
+    {
+        int count = 0;
+        for (int i = 0; i <= 100; i++){
+            if(i % 3 == 0 || i % 5 ==0)
+            {
+                count += i;
+            }
+            else if(i % 15 == 0)
+            {
+                count -= i;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
