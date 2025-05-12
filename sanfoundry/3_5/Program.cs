70b30c2e627261d3e239496837ad7f7bfb578c3e
similar86 <40771801+similar86@users.noreply.github.com>
2025-05-12 14:34:23 +0900

    値の変更

--- sanfoundry/3_5/Program.cs
+++ sanfoundry/3_5/Program.cs
@@ -6,7 +6,7 @@ class program
     public static void Main()
     {
         int count = 0;
-        for (int i = 0; i <= 100; i++){
+        for (int i = 0; i < 100; i++){
             if(i % 3 == 0 || i % 5 ==0)
             {
                 count += i;
